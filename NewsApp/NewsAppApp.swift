534a8489dd8d4882577c7787933f9594d0b3657d
aniruddhadesai <aniruddha.desai@sodelsolutions.com>
2025-03-22 00:57:36 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by shaikh faizan on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
